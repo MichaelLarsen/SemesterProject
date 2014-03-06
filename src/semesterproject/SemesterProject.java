51fd67f9410d3b528162fb8ca71e3573654d528e
Michael <Michael@10.18.137.104>
2014-03-06 13:36:49 +0100

    Initialize Project

---
+++ src/semesterproject/SemesterProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterproject;
+
+/**
+ *
+ * @author Michael
+ */
+public class SemesterProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
