d1dec435dc98886f896a42c9e6c4670fe19afe85
Darkeonz <Darkeonz@Darkeonz-Pc>
2014-03-19 17:07:09 +0100

    DBConnector loader driveren og skaber connection med brugernavn og password til databasen

--- src/dataSource/DBConnector.java
+++ src/dataSource/DBConnector.java
@@ -3,15 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  *
- * @author Seb
+ * @author ANDREAS!!! og lidt sebastian og michael
  */
 public class DBConnector
 {
+
     private static String id = "SEM2_TEST_GR04";
     private static String pw = "SEM2_TEST_GR04";
+
+    public Connection getConnection()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@delfi.lyngbyes.dk:1521:KNORD", id, pw);
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Did not load driver or username/password correctly!");
+            System.out.println("error in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+
+        return con;
+    }
+
+    public void releaseConnection(Connection con)
+    {
+        try
+        {
+            con.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
+    }
 }
